c1f6be1acdc8957644bf57c3dfa4c0b18ec2bf45
Mvsmith81 <130387800+Mvsmith81@users.noreply.github.com>
2026-09-13 07:56:24 -0400

    Refresh offline cache for cloud release

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bodysmith-v4';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='bodysmith-v5';
+const SHELL=['./','./index.html','./styles.css?v=20260913','./app.js?v=20260913','./manifest.webmanifest?v=20260913','./icon.svg?v=20260913'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
